cf2c1e6a7532bbe581394360b2d9021fd29ead58
Ismail Cherri <i.cherri@purpose.hr>
2024-10-08 23:39:12 +0200

    Added header and main content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import { DM_Sans } from 'next/font/google'
 import './globals.css'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+// If loading a variable font, you don't need to specify the font weight
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.className} flex flex-col min-h-screen`}>
         {children}
       </body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
 }
 
 export default nextConfig
